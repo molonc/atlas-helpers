be8760ca3e5880563560c76f791b643963a8b1ce
Robert Reinert <robert.reinert@gmail.com>
2023-02-08 15:41:20 -0800

    add exports to config

--- config/api.js
+++ config/api.js
@@ -1,3 +1,3 @@
-const baseUrl = process.env.ATLAS_BASE_URL;
-const apiVer = process.env.ATLAS_API_VERSION;
-const labgroup = process.env.ATLAS_LAB_GROUP;
+export const baseUrl = process.env.ATLAS_BASE_URL;
+export const apiVer = process.env.ATLAS_API_VERSION;
+export const labgroup = process.env.ATLAS_LAB_GROUP;
