bff012fd02ecad38b97bd1b646fa356dd3db64f8
Robert Reinert <robert.reinert@gmail.com>
2023-02-08 15:37:50 -0800

    First commit

---
+++ config/api.js
@@ -0,0 +1,3 @@
+const baseUrl = process.env.ATLAS_BASE_URL;
+const apiVer = process.env.ATLAS_API_VERSION;
+const labgroup = process.env.ATLAS_LAB_GROUP;
---
+++ const/enums.js
@@ -0,0 +1,35 @@
+export const SAMPLE_MODULES_ENUM = [
+  "DNA__RNA",
+  "Tissue",
+  "blocks__slides",
+  "Plasmas__Buffy_coats",
+  "cell__lines",
+  "plasmid"
+]
+
+export const EXPERIMENT_MODULES_ENUM = [
+  "sulfitesequencing",
+  "cellenone",
+  "chipsequencing",
+  "digitalpathology",
+  "exomesequencing",
+  "merfish",
+  "mirnasequencing",
+  "nanostring",
+  "nanowell",
+  "platereading",
+  "spcsequencing",
+  "targetedsequencing",
+  "wholegenomesequencing",
+  "wholegenomesequencinglongread",
+  "wholetranscriptomesequencing"
+];
+
+export const ANALYSIS_MODULES_ENUM = [
+  "dlp",
+  "merfish",
+  "other",
+  "tenx",
+  "wholegenomesequencing",
+  "wholetranscriptomesequencing",
+];
