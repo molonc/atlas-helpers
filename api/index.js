4fa05868f736d2fbba82a580a39274ba9f883fbb
Robert Reinert <robert.reinert@gmail.com>
2023-02-21 12:39:36 -0800

    removed config from module

--- api/index.js
+++ api/index.js
@@ -1,77 +1,76 @@
 import axios from 'axios';
 import { SAMPLE_MODULES_ENUM, ANALYSIS_MODULES_ENUM } from '../const/enums.js';
-import { baseUrl, apiVer, labGroup } from '../config/api.js';
 
-export const apiUrl = (baseUrl, apiVer) => `${baseUrl}/${apiVer}`
+export const atlasApiUrl = (baseUrl, apiVer) => `${baseUrl}/${apiVer}`
 
-export const auth = async (username, password) => {
-  const tokenRes = await axios.post(`${baseUrl}/${apiVer}/auth/login/`, { username: username, password: password });
+export const auth = async (username, password, atlasApiUrl) => {
+  const tokenRes = await axios.post(`${atlasApiUrl}/auth/login/`, { username: username, password: password });
   return `Bearer ${tokenRes.data.access}`;
 }
 
-export const getSample = async (sampleId, token) => {
-  const promises = SAMPLE_MODULES_ENUM.map(element => axios.get(`${baseUrl}/${apiVer}/${labGroup}/samples/${element}?by_keywords=${sampleId}&search_on=s_uid`,
+export const getSample = async (sampleId, token, atlasApiUrl) => {
+  const promises = SAMPLE_MODULES_ENUM.map(element => axios.get(`${atlasApiUrl}/${labGroup}/samples/${element}?by_keywords=${sampleId}&search_on=s_uid`,
     { headers: { Authorization: token } }
   ));
   const res = await Promise.all(promises);
   return res.flatMap(element => element.data.data);
 }
 
-export const getExperiment = (experimentId, token) => {
-  return axios.get(`${baseUrl}/${apiVer}/${labGroup}/xp/${experimentId}/`,
+export const getExperiment = (experimentId, token, atlasApiUrl) => {
+  return axios.get(`${atlasApiUrl}/${labGroup}/xp/${experimentId}/`,
     { headers: { Authorization: token } }
   );
 }
 
-export const getAnalysis = async (analysisId, token) => {
-  const promises = ANALYSIS_MODULES_ENUM.map(element => axios.get(`${baseUrl}/${apiVer}/${labGroup}/ax/AX${element}?by_keywords=${analysisId}&search_on=s_uid`,
+export const getAnalysis = async (analysisId, token, atlasApiUrl) => {
+  const promises = ANALYSIS_MODULES_ENUM.map(element => axios.get(`${atlasApiUrl}/${labGroup}/ax/AX${element}?by_keywords=${analysisId}&search_on=s_uid`,
     { headers: { Authorization: token } }
   ));
   const res = await Promise.all(promises);
   return res.flatMap(element => element.data.data);
 }
 
-export const getExperimentbyExternalId = (externalId, experimentType, token) => {
-  return axios.get(`${baseUrl}/${apiVer}/${labGroup}/xp/${experimentType}?by_keywords=${externalId}&search_on=s_external_identifier`,
+export const getExperimentbyExternalId = (externalId, experimentType, token, atlasApiUrl) => {
+  return axios.get(`${atlasApiUrl}/${labGroup}/xp/${experimentType}?by_keywords=${externalId}&search_on=s_external_identifier`,
     { headers: { Authorization: token } }
   );
 }
 
-export const addSample = (sample, sampleType, token) => {
-  return axios.post(`${baseUrl}/${apiVer}/${labGroup}/samples/${sampleType}/`,
+export const addSample = (sample, sampleType, token, atlasApiUrl) => {
+  return axios.post(`${atlasApiUrl}/${labGroup}/samples/${sampleType}/`,
     sample,
     { headers: { Authorization: token } }
   );
 }
 
-export const addExperiment = (experiment, experimentType, token) => {
-  return axios.post(`${baseUrl}/${apiVer}/${labGroup}/xp/${experimentType}/`,
+export const addExperiment = (experiment, experimentType, token, atlasApiUrl) => {
+  return axios.post(`${atlasApiUrl}/${labGroup}/xp/${experimentType}/`,
     experiment,
     { headers: { Authorization: token } }
   );
 }
 
-export const addAnalysis = (analysis, analysisType, token) => {
-  return axios.post(`${baseUrl}/${apiVer}/${labGroup}/ax/${analysisType}/`,
+export const addAnalysis = (analysis, analysisType, token, atlasApiUrl) => {
+  return axios.post(`${atlasApiUrl}/${labGroup}/ax/${analysisType}/`,
     analysis,
     { headers: { Authorization: token } }
   );
 }
 
-export const deleteSample = (sampleId, sampleType, token) => {
-  return axios.delete(`${baseUrl}/${apiVer}/${labGroup}/samples/${sampleType}/${sampleId}`,
+export const deleteSample = (sampleId, sampleType, token, atlasApiUrl) => {
+  return axios.delete(`${atlasApiUrl}/${labGroup}/samples/${sampleType}/${sampleId}`,
     { headers: { Authorization: token } }
   );
 }
 
-export const deleteExperiment = (experimentId, experimentType, token) => {
-  return axios.delete(`${baseUrl}/${apiVer}/${labGroup}/xp/${experimentType}/${experimentId}`,
+export const deleteExperiment = (experimentId, experimentType, token, atlasApiUrl) => {
+  return axios.delete(`${atlasApiUrl}/${labGroup}/xp/${experimentType}/${experimentId}`,
     { headers: { Authorization: token } }
   );
 }
 
-export const deleteAnalysis = (analysisId, analysisType, token) => {
-  return axios.delete(`${baseUrl}/${apiVer}/${labGroup}/ax/${analysisType}/${analysisId}`,
+export const deleteAnalysis = (analysisId, analysisType, token, atlasApiUrl) => {
+  return axios.delete(`${atlasApiUrl}/${labGroup}/ax/${analysisType}/${analysisId}`,
     { headers: { Authorization: token } }
   );
 }
--- config/api.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl = process.env.ATLAS_BASE_URL;
-export const apiVer = process.env.ATLAS_API_VERSION;
-export const labGroup = process.env.ATLAS_LAB_GROUP;
